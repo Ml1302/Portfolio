1c27e62d33c884a0534850e802cacbb4cbd418f1
Ml1302 <miguel.es.ri1302@gmail.com>
2024-07-29 23:28:57 -0500

    Borrando todo los archivos relacionados a persona

--- src/main/java/com/example/demo/interfaces/IPersona.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.interfaces;
-
-import com.example.demo.model.Persona;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IPersona extends CrudRepository<Persona, Integer> {
-}
